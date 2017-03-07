a2a40ce0842f405e58338497e64545ea01789b00
ggarber <gregory.garber@yahoo.com>
2017-03-07 12:02:19 -0500

    added basic authentication with devise gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+
   protect_from_forgery with: :exception
+
+  before_action :authenticate_user!    # devise
 
   GENERIC_FORM_ERROR_FLASH = 'Please correct errors below and re-submit.'
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,6 +39,9 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
+  # default_url_options needed by devise gem
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   Paperclip.options[:command_path] = '/usr/bin/convert'
 
 end
